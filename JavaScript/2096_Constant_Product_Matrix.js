eda6f56831ad192fad417008bee36d5203ebe4ca
Ronak1605 <rpat256@aucklanduni.ac.nz>
2026-03-25 13:17:54 +1300

    Submitted Solution for Constant Product Matrix

--- JavaScript/2096_Constant_Product_Matrix.js
+++ JavaScript/2096_Constant_Product_Matrix.js
@@ -1,6 +1,6 @@
 // Problem: 2096 (Leetcode) - Constant Product Matrix, Daily Challenge 2026-03-24
 
-// Solution Status: WIP
+// Solution Status: Submitted at Mar 25, 2026 13:16 NZST, Beats 85.71% of solutions for run-time and 14.29% for Memory
 
 /* Problem Description:
 *    Given a 0-indexed 2D integer matrix grid of size n * m, we define a 0-indexed 2D matrix p of size n * m as the product matrix of grid if the following condition is met:
@@ -52,26 +52,47 @@ var constructProductMatrix = function(grid) {
     const modValue = 12345;
     const gridLength = grid.length;
     const gridHeight = grid[0].length; // As the grid has a size of n * m, all heights (i.e., length of sub-arrays) are the same
+    const flatGrid = grid.flat(); // Flatten 2-D grid to calculate prefix and suffix product arrays easier
+    const flatGridLength = flatGrid.length;
 
-    let totalProduct = 1;
+    let prefixProductArray = new Array(flatGridLength).fill(1);
+    let suffixProductArray = new Array(flatGridLength).fill(1);
     let productMatrix = Array.from(Array(gridLength), () => new Array(gridHeight));
 
-    
+    // Build prefix products: product of all values before index i.
+    for (let i = 1; i < flatGridLength; i++) {
+        prefixProductArray[i] = (prefixProductArray[i - 1] * (flatGrid[i - 1] % modValue)) % modValue;
+    }
+
+    // Build suffix products: product of all values after index i.
+    for (let i = flatGridLength - 2; i >= 0; i--) {
+        suffixProductArray[i] = (suffixProductArray[i + 1] * (flatGrid[i + 1] % modValue)) % modValue;
+    }
+
+    // Convert flat index back to 2-D index and populate answer matrix.
+    for (let i = 0; i < flatGridLength; i++) {
+        let gridLengthIndex = Math.floor(i / gridHeight);
+        let gridHeightIndex = i % gridHeight;
+
+        productMatrix[gridLengthIndex][gridHeightIndex] = (prefixProductArray[i] * suffixProductArray[i]) % modValue;
+    }
+
+    return productMatrix;
 };
 
 function runTestCases() {
     const testCases = [
-        // Passing
+        // Status = Passed
         {
             input: [[1,2],[3,4]],
             expected: [[24,12],[8,6]]
         },
-        // Passing
+        // Status = Passed
         {
             input: [[12345],[2],[1]],
             expected: [[2],[0],[0]]
         },
-        // Failing
+        // Status = Passed
         {
             input: [[8,18],[24,20],[9,5],[26,26],[19,19],[20,1],[20,23],[15,19],[24,14],[12,15],[22,3],[22,11],[9,25]],
             expected: [[11625,3795],[12105,4650],[7590,6255],[8325,8325],[4245,4245],[4650,6585],[4650,2970],[2085,4245],[12105,10170],[11865,2085],[3105,10425],[3105,6210],[7590,3720]]
